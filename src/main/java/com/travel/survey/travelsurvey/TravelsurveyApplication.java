f5f5d4e4a1beb73638c85bde25e96f0bf8c9d47c
kirankumar0604@gmail.com <kirankumar0604@gmail.com>
2024-01-31 23:38:56 +0530

    Added new files

---
+++ src/main/java/com/travel/survey/travelsurvey/TravelsurveyApplication.java
@@ -0,0 +1,13 @@
+package com.travel.survey.travelsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travel/survey/travelsurvey/TravelsurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.survey.travelsurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
